30ac475c66c90fcaa768a5f511bf6ccf09d05f42
Felipe <felipe.edfreire@gmail.com>
2023-09-26 16:21:33 -0300

    feat: initial structure for file transfer

--- application/src/app/page.js
+++ application/src/app/page.js
@@ -1,95 +1,94 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import { API_URL, ON_UPLOAD_EVENT } from "@/utils/config";
 
 export default function Home() {
+  const [bytesAmount, setBytesAmount] = useState(0);
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    const ioClient = io.connect(API_URL, { withCredentials: false });
+    ioClient.on("connect", (msg) => {
+      console.log("connected!", ioClient.id);
+      const targetUrl = API_URL + `?socketId=${ioClient.id}`;
+      configureForm(targetUrl);
+    });
+
+    ioClient.on(ON_UPLOAD_EVENT, (bytesReceived) => {
+      console.log("received", bytesReceived);
+      bytesAmount = bytesAmount - bytesReceived;
+      setBytesAmount(bytesAmount);
+    });
+  }, []);
+
+  const configureForm = (targetUrl) => {
+    formRef.current.action = targetUrl;
+  };
+
+  const formatBytes = (bytes, decimals = 2) => {
+    if (bytes === 0) return "0 Bytes";
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+  };
+
+  function showSize() {
+    const { files: fileElements } = document.getElementById("file");
+    if (!fileElements.length) return;
+
+    const files = Array.from(fileElements);
+    const { size } = files.reduce(
+      (prev, next) => ({ size: prev.size + next.size }),
+      { size: 0 }
+    );
+
+    setBytesAmount(size);
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <div className="row">
+      <div className="col-md-4">
+        <h3>Uploading files</h3>
+        <form
+          id="form"
+          method="POST"
+          encType="multipart/form-data"
+          ref={formRef}
+        >
+          <div className="form-group">
+            <label htmlFor="field">Files</label>
+            <input
+              type="file"
+              className="form-control"
+              id="file"
+              name="file"
+              onChange={showSize}
+              multiple
             />
-          </a>
-        </div>
+            <output className="span" id="size">
+              Pending Bytes to Upload:{" "}
+              <strong>{formatBytes(bytesAmount)}</strong>
+            </output>
+            <br />
+            <output className="span" id="msg"></output>
+          </div>
+          <div className="form-group">
+            <input type="submit" className="btn btn-success" />
+            <input
+              type="reset"
+              className="btn btn-danger"
+              onClick={() => {
+                setBytesAmount(0);
+              }}
+            />
+          </div>
+        </form>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
---
+++ application/src/utils/config.js
@@ -0,0 +1,2 @@
+export const API_URL = "http://localhost:8000";
+export const ON_UPLOAD_EVENT = "file-uploaded";
